4064410cbbd5ed2fc567d1f09315db3d74e08129
Martin Herweg <martin@martinherweg.de>
2017-12-31 11:42:37 +0100

    ported controller and plugin stuff to Craft 3

--- src/Craft3Gonzo.php
+++ src/Craft3Gonzo.php
@@ -10,14 +10,13 @@
 
 namespace martinherweg\craft3gonzo;
 
-
 use Craft;
 use craft\base\Plugin;
 use craft\services\Plugins;
 use craft\events\PluginEvent;
 use craft\web\UrlManager;
 use craft\events\RegisterUrlRulesEvent;
-
+use martinherweg\craft3gonzo\controllers\DefaultController;
 use yii\base\Event;
 
 /**
@@ -62,6 +61,10 @@ class Craft3Gonzo extends Plugin
      * you do not need to load it in your init() method.
      *
      */
+
+    public $controllerMap = [
+      'template-index' => DefaultController::class
+    ];
     public function init()
     {
         parent::init();
@@ -72,16 +75,8 @@ class Craft3Gonzo extends Plugin
             UrlManager::class,
             UrlManager::EVENT_REGISTER_SITE_URL_RULES,
             function (RegisterUrlRulesEvent $event) {
-                $event->rules['siteActionTrigger1'] = 'craft-3-gonzo/default';
-            }
-        );
-
-        // Register our CP routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'craft-3-gonzo/default/do-something';
+                $event->rules['styleguide'] = 'craft-3-gonzo/default/template-index';
+                $event->rules['styleguide/<myVariable:S+>'] = 'craft-3-gonzo/templateRender';
             }
         );
 
@@ -96,24 +91,24 @@ class Craft3Gonzo extends Plugin
             }
         );
 
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
+        /**
+         * Logging in Craft involves using one of the following methods:
+         *
+         * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+         * Craft::info(): record a message that conveys some useful information.
+         * Craft::warning(): record a warning message that indicates something unexpected has happened.
+         * Craft::error(): record a fatal error that should be investigated as soon as possible.
+         *
+         * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+         *
+         * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+         * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+         *
+         * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+         * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+         *
+         * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
+         */
         Craft::info(
             Craft::t(
                 'craft-3-gonzo',
@@ -126,5 +121,4 @@ class Craft3Gonzo extends Plugin
 
     // Protected Methods
     // =========================================================================
-
 }
--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -10,10 +10,11 @@
 
 namespace martinherweg\craft3gonzo\controllers;
 
+use craft\web\View;
 use martinherweg\craft3gonzo\Craft3Gonzo;
-
 use Craft;
 use craft\web\Controller;
+use Illuminate\Support\Collection;
 
 /**
  * Default Controller
@@ -37,16 +38,17 @@ use craft\web\Controller;
  */
 class DefaultController extends Controller
 {
-
     // Protected Properties
     // =========================================================================
 
+    protected $templatesPath;
+    protected $searchFolders;
     /**
      * @var    bool|array Allows anonymous access to this controller's actions.
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = ['index', 'do-something'];
+    protected $allowAnonymous = ['template-index', 'template-render'];
 
     // Public Methods
     // =========================================================================
@@ -57,23 +59,136 @@ class DefaultController extends Controller
      *
      * @return mixed
      */
-    public function actionIndex()
+    public function init()
     {
-        $result = 'Welcome to the DefaultController actionIndex() method';
-
-        return $result;
+        $this->templatesPath = \Craft::$app->view->getTemplatesPath();
+        $this->searchFolders = Craft3Gonzo::$plugin->getSettings('searchFolders') ? Craft3Gonzo::$plugin->getSettings('searchFolders') : [];
     }
 
     /**
-     * Handle a request going to our plugin's actionDoSomething URL,
-     * e.g.: actions/craft-3-gonzo/default/do-something
+     * Creates a tree-structured array of directories and files from a given root folder.
      *
-     * @return mixed
+     * Gleaned from: http://stackoverflow.com/questions/952263/deep-recursive-array-of-directory-structure-in-php
+     *
+     * @param string $dir
+     * @param string $regex
+     * @param boolean $ignoreEmpty Do not add empty directories to the tree
+     * @return array
      */
-    public function actionDoSomething()
+    public function dirtree($dir, $regex = '', $ignoreEmpty = false, $depth = 0)
     {
-        $result = 'Welcome to the DefaultController actionDoSomething() method';
+        // https://stackoverflow.com/questions/18251505/file-structure-to-multidimensional-array
+        $origin = new \IteratorIterator(new \DirectoryIterator($dir));
+        $dirs = [];
+        $config = null;
 
-        return $result;
+        foreach ($origin as $item) {
+            if ($item->isDot()) {
+                continue;
+            }
+
+            $searchFolders = count($this->searchFolders) ? in_array($item->getFilename(), $this->searchFolders) : true;
+
+            if ($item->isDir() && $searchFolders) {
+                $obj = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($item->getPathname(), \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::CHILD_FIRST);
+                $arr = [];
+                foreach ($obj as $info) {
+                    $path = $info->isDir() ? [$info->getFilename() => [
+                        'config' => collect($this->getConfig($info->getRealPath()))
+                            ->filter(function ($value, $key) {
+                                return $value != null;
+                            })->toArray()
+                    ]] : [$info->getFilename()];
+
+                    for ($depth = $obj->getDepth() - 1; $depth >= 0; $depth--) {
+                        $templates = [];
+                        $assets = [];
+                        $basename = pathinfo($info)['filename'];
+                        $name = $info->getFilename();
+                        $fullPath = $info->getRealPath();
+
+                        $file = [
+                            'name' => $basename,
+                            'extension' => $info->getExtension(),
+                            'filename' => $name,
+                            'fullPath' => $fullPath,
+                            'relativePath' => str_replace($this->templatesPath, '', $fullPath),
+                        ];
+
+                        switch ($info->getExtension()) {
+                            case 'html':
+                            case 'twig':
+                                $templates[] = $file;
+                                break;
+                            case 'js':
+                            case 'css':
+                            case 'vue':
+                            case 'scss':
+                                $assets[] = $file;
+                                break;
+                        }
+                        $path = [$obj->getSubIterator($depth)->current()->getFilename() => [
+                            'templates' => $templates,
+                            'assets' => $assets,
+                        ]];
+                    }
+                    $arr = array_merge_recursive($arr, $path);
+                }
+                $dirs[$item->getFilename()]['children'] = $arr;
+            }
+        }
+
+        $collection = new Collection($dirs);
+        return $collection;
+    }
+
+    public function getConfig(string $path)
+    {
+        $options = new Collection();
+        $mergedOptions = collect();
+        $configFiles = glob($path . '/*config*');
+        foreach ($configFiles as $config) {
+            if (pathinfo($config, PATHINFO_EXTENSION) === 'json') {
+                $json = file_get_contents($config);
+                $jsonVariables = json_decode($json, true);
+                $jsonVariables = collect($jsonVariables);
+                $options = $options->merge($jsonVariables);
+            }
+            if (pathinfo($config, PATHINFO_EXTENSION) === 'php') {
+                include_once $config;
+                if (isset($opt)) {
+                    $mergedOptions = $options->merge($opt);
+                }
+            }
+        }
+
+        return $mergedOptions->isNotEmpty() ? $mergedOptions->toArray() : $options->toArray();
+    }
+
+    public function actionTemplateIndex()
+    {
+        $treeView = $this->dirtree($this->templatesPath, '', true);
+        $variables['templates'] = $treeView->toArray();
+        $oldMode = \Craft::$app->view->getTemplateMode();
+        \Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
+        $html = \Craft::$app->view->renderTemplate('craft-3-gonzo/styleguide.twig', $variables);
+        \Craft::$app->view->setTemplateMode($oldMode);
+        return $html;
+    }
+
+
+    /**
+     * Render given Module in StyleguideRenderer Template
+     * @param array $variables
+     * @return \yii\web\Response
+     */
+    public function actionTemplateRender(array $variables)
+    {
+        $pathinfo = pathinfo($variables['myVariable']);
+        $modulePath = $this->templatesPath . $pathinfo['dirname'];
+        $variables['component'] = $variables['myVariable'];
+        $variables['templateOptions'] = $this->getConfig($modulePath);
+
+        return $this->renderTemplate('styleguideRenderer', $variables);
     }
 }
