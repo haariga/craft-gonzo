615529a23f07fb9cca9d353563ca3617ebf143dd
Martin Herweg <martin@martinherweg.de>
2018-02-07 21:58:07 +0100

    renamed to patternlib

--- frontEndSources/App.vue
+++ frontEndSources/App.vue
@@ -13,65 +13,66 @@
     </div>
 </template>
 
-<script>
-    import Filelist from 'Modules/Filelist/Filelist'
-    import PreviewArea from 'Modules/PreviewArea/PreviewArea'
 
-    export default {
-        name: 'Styleguide',
-        data() {
-            return {
-                sidebar: true
-            }
-        },
-        components: {
-            Filelist,
-            PreviewArea,
-        },
-        computed: {
-            filelist() {
-                return this.$store.getters.filelist
-            },
-        },
-        mounted() {
-            this.$store.dispatch('setFilelist', window.filelist)
-        },
+<script>
+import Filelist from 'Modules/Filelist/Filelist';
+import PreviewArea from 'Modules/PreviewArea/PreviewArea';
+
+export default {
+  name: 'patternlib',
+  data() {
+    return {
+      sidebar: true
+    };
+  },
+  components: {
+    Filelist,
+    PreviewArea
+  },
+  computed: {
+    filelist() {
+      return this.$store.getters.filelist;
     }
+  },
+  mounted() {
+    this.$store.dispatch('setFilelist', window.filelist);
+  }
+};
 </script>
 
 <style lang="scss">
-    .pl-wrapper {
-        width: 100vw;
-        max-width: 100%;
-        height: 100vh;
-        overflow: hidden;
-        background-color: #f8f8f8;
-        display: flex;
-        flex-flow: row wrap;
-    }
+.pl-wrapper {
+  width: 100vw;
+  max-width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #f8f8f8;
+  display: flex;
+  flex-flow: row wrap;
+}
 
-    .pl-aside {
-        width: 320px;
-        height: 100vh;
-        overflow-y: scroll;
-        overflow-x: hidden;
-        -webkit-overflow-scrolling: touch;
-    }
+.pl-aside {
+  width: 320px;
+  height: 100vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+}
 
-    .pl-main {
-        width: calc(100vw - 320px);
-        padding: 32px 32px 64px 32px;
-        overflow-y: scroll;
-        overflow-x: hidden;
-        -webkit-overflow-scrolling: touch;
-    }
+.pl-main {
+  width: calc(100vw - 320px);
+  padding: 32px 32px 64px 32px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  -webkit-overflow-scrolling: touch;
+}
 
-    .pl-main__inner {
-        width: 100%;
-        border-radius: 10px;
-        overflow: hidden;
-        padding: 32px;
-        background-color: #fff;
-        box-shadow: 0 2px 0 rgba(#aaa, 0.1), 0 1px 2px rgba(#000, 0.1);
-    }
+.pl-main__inner {
+  width: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  padding: 32px;
+  background-color: #fff;
+  box-shadow: 0 2px 0 rgba(#aaa, 0.1), 0 1px 2px rgba(#000, 0.1);
+}
 </style>
