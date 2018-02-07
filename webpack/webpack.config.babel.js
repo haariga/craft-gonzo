c054546b8b8047ef97c85dff2ea45a36239a3167
Martin Herweg <martin@martinherweg.de>
2018-02-07 21:42:31 +0100

    add scss loader foo

--- webpack/webpack.config.babel.js
+++ webpack/webpack.config.babel.js
@@ -1,7 +1,26 @@
 import path from 'path';
 import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const absPath = dir => path.resolve(__dirname, `../${dir}`);
+
+const CSS_STACK = ({ scss = true, vue = false, sassResources = false } = {}) => [
+  {
+    loader: vue ? 'vue-style-loader' : 'style-loader',
+  },
+  {
+    loader: 'css-loader'
+  },
+  ...(scss ? [{
+    loader: 'sass-loader'
+  }] : {}),
+  ...(sassResources ? [{
+    loader: 'sass-resoures-loader',
+    options: {
+      resources: [absPath('frontEndSources/scss/_variables.scss'), absPath('frontEndSources/scss/_mixins.scss'), absPath('frontEndSources/scss/_functions.scss')]
+    }
+  }] : [])
+];
 
 const webpackConfig = env => ({
   devtool: 'cheap-eval-source-map',
@@ -12,13 +31,13 @@ const webpackConfig = env => ({
     path: absPath('src/assetbundles/gonzo/dist/'),
     publicPath: '',
     filename: 'js/Craft3Gonzo.js',
-    chunkFilename: 'js/[id].js'
+    chunkFilename: 'js/[id].js',
   },
   devServer: {
     contentBase: absPath('resources/'),
-    https: true,
     hot: true,
     inline: true,
+    overlay: true,
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
@@ -45,9 +64,18 @@ const webpackConfig = env => ({
         include: absPath('frontEndSources/'),
         options: {
           loaders: {
-            scss: [{ loader: 'vue-style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+            scss: CSS_STACK({ vue: true }),
+            css: CSS_STACK({ scss: false, vue: true }),
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        include: absPath('frontEndSources/'),
+        use: env === 'production' ? ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: CSS_STACK(),
+        }) : CSS_STACK(),
       },
       {
         test: /\.js$/,
@@ -56,7 +84,12 @@ const webpackConfig = env => ({
       }
     ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.ProgressPlugin(), new webpack.NamedModulesPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.ProgressPlugin(), new webpack.NamedModulesPlugin(),
+            ...(env === 'production' ? [new ExtractTextPlugin({
+              filename: 'css/Craft3Gonzo.css',
+              allChunks: true,
+            })] : [])
+  ]
 });
 
 export default webpackConfig;
