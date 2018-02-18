09a35a87b2e20ab6124c4d054f2a0b94632ae21e
Martin Herweg <martin@martinherweg.de>
2018-02-18 16:24:11 +0100

    updated ferdi

--- .ferdirc.js
+++ .ferdirc.js
@@ -1,20 +1,19 @@
-const defaults = {
-  template: false,
-  css: false,
-  javascript: false,
-  vue: true,
-  fractal: false
-};
-
 module.exports = {
+  defaults: {
+    template: false,
+    css: false,
+    javascript: false,
+    vue: true,
+    fractal: false,
+  },
   fileHeader: {
     authors: [
       {
         name: 'Martin Herweg',
-        email: 'martin@herweg.co'
-      }
+        email: 'martin@herweg.co',
+      },
     ],
-    projectName: 'Gonzo'
+    projectName: 'Gonzo',
   },
   files: {
     template: {
@@ -22,42 +21,35 @@ module.exports = {
       postfix: 'template',
       extension: 'html',
       description: 'ferdi should create a Template File',
-      default: defaults.template
     },
     css: {
       name: '',
       postfix: 'style',
       extension: 'scss',
       description: 'ferdi should create Stylesheet File',
-      default: defaults.css
     },
     javascript: {
       name: '',
       postfix: 'script',
       extension: 'js',
       description: 'ferdi should create JavaScript File',
-      default: defaults.javascript
     },
     vue: {
       name: '',
       postfix: '',
       extension: 'vue',
-      default: defaults.vue
     },
     fractal: {
       name: '',
       postfix: 'config',
       extension: 'js',
-      default: defaults.fractal
-    }
+    },
   },
   paths: {
     templateBase: 'tmpl/',
-    modulePath: 'src/components/',
+    modulePath: 'frontEndSources/components/',
     pathOptions: {
       // the first character of each key works as an alias for the path so you could use `-a` as an alias for atomic
-    }
-  }
+    },
+  },
 };
-
-exports.defaults = defaults;
