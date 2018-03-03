b13f6611d06a3946ae7f6fa39b743ea063ad1f64
Martin Herweg <martin@martinherweg.de>
2018-03-03 06:14:03 +0100

    copy an example renderer to craft templates folder
    # Conflicts:
    #	src/templates/patternlibRenderer.twig

--- src/CraftGonzo.php
+++ src/CraftGonzo.php
@@ -63,72 +63,72 @@
          */
 
         public $controllerMap = [
-            'template-index'  => DefaultController::class,
-            'template-render' => DefaultController::class,
+            'template-index'   => DefaultController::class,
+            'template-render'  => DefaultController::class,
             'get-file-content' => DefaultController::class,
-    ];
+        ];
 
-    public function init()
-    {
-        parent::init();
-        self::$plugin = $this;
+        public function init()
+        {
+            parent::init();
+            self::$plugin = $this;
 
-        // Register our site routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['patternlib'] = 'craft-gonzo/default/template-index';
-                $event->rules['patternlib/getfilecontent/<file:\S+>'] = 'craft-gonzo/default/get-file-content';
-                $event->rules['patternlib/getfilerender'] = 'craft-gonzo/default/get-file-render';
-                $event->rules['patternlib/<component:.+?>/variant/<variant:[^\/]+$>/'] = 'craft-gonzo/default/template-render';
-            }
-        );
-
-        // Do something after we're installed
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                    // We were just installed
+            // Register our site routes
+            Event::on(
+                UrlManager::class,
+                UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+                function (RegisterUrlRulesEvent $event) {
+                    $event->rules['patternlib'] = 'craft-gonzo/default/template-index';
+                    $event->rules['patternlib/getfilecontent/<file:\S+>'] = 'craft-gonzo/default/get-file-content';
+                    $event->rules['patternlib/getfilerender'] = 'craft-gonzo/default/get-file-render';
+                    $event->rules['patternlib/<component:.+?>/variant/<variant:[^\/]+$>/'] = 'craft-gonzo/default/template-render';
                 }
-            }
-        );
+            );
 
-        /**
-         * Logging in Craft involves using one of the following methods:
-         *
-         * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
-         * Craft::info(): record a message that conveys some useful information.
-         * Craft::warning(): record a warning message that indicates something unexpected has happened.
-         * Craft::error(): record a fatal error that should be investigated as soon as possible.
-         *
-         * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
-         *
-         * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
-         * the category to the method (prefixed with the fully qualified class name) where the constant appears.
-         *
-         * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
-         * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
-         *
-         * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
-         */
-        Craft::info(
-            Craft::t(
-                'craft-gonzo',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
-    }
+            // Do something after we're installed
+            Event::on(
+                Plugins::class,
+                Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+                function (PluginEvent $event) {
+                    if ($event->plugin === $this) {
+                        copy($this->basePath . '/templates/patternlibRenderer.twig',
+                            CRAFT_BASE_PATH . '/templates/patternlib/index.example.html');
+                    }
+                }
+            );
 
-    // Protected Methods
-    // =========================================================================
+            /**
+             * Logging in Craft involves using one of the following methods:
+             *
+             * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+             * Craft::info(): record a message that conveys some useful information.
+             * Craft::warning(): record a warning message that indicates something unexpected has happened.
+             * Craft::error(): record a fatal error that should be investigated as soon as possible.
+             *
+             * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+             *
+             * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+             * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+             *
+             * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+             * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+             *
+             * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
+             */
+            Craft::info(
+                Craft::t(
+                    'craft-gonzo',
+                    '{name} plugin loaded',
+                    ['name' => $this->name]
+                ),
+                __METHOD__
+            );
+        }
+
+        // Protected Methods
+        // =========================================================================
         protected function createSettingsModel()
         {
             return new models\Settings();
         }
-
-}
+    }
