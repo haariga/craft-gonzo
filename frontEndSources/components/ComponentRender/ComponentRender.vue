178fca0a19802bbe2c1610eec33caf7efdb62040
David Hellmann <davidhellmann.com@gmail.com>
2018-07-28 03:50:35 +0200

    Open New tab

--- frontEndSources/components/ComponentRender/ComponentRender.vue
+++ frontEndSources/components/ComponentRender/ComponentRender.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="pl-content">
     <div class="pl-content__section">
-      <!--<h2 class="pl-headline&#45;&#45;h5">Code: SCSS</h2>-->
       <div class="pl-content__container  pl-content__container--iframeActions">
         <div class="pl-buttonGroup  pl-buttonGroup--pill">
           <button v-for="width in mqButtons"
@@ -10,31 +9,36 @@
                   class="pl-button pl-button--pill"
                   @click="iFrameWidth(width)"
                   v-text="`${width}`"/>
+
+          <a :href="frame.src" class="pl-button pl-button--newWindow" target="_blank">Open in new tab</a>
         </div>
       </div>
-      <div v-show="iFrame.loaded" class="pl-content__container  pl-content__container--iframe">
-        <iframe v-if="activeTemplate.relativePath"
-                ref="iframe"
-                :style="{ 'max-width': iFrame.width }"
-                :src="frame.src"
-                :height="iFrame.height"
-                marginheight="0"
-                marginwidth="0"
-                vspace="0"
-                hspace="0"
-                scrolling="yes"
-                name="patternlibRenderer"
-                frameborder="0"
-                @load="iFrameSize"
-        />
-        <div v-else>
-          Please choose a component
-        </div>
+      <div :class="{'pl-content__container--loaded': iFrame.loaded}" class="pl-content__container  pl-content__container--iframe">
+        <transition name="fade">
+          <iframe v-if="activeTemplate.relativePath"
+                  v-show="iFrame.loaded"
+                  ref="iframe"
+                  :style="{ 'max-width': iFrame.width }"
+                  :src="frame.src"
+                  :height="iFrame.height"
+                  marginheight="0"
+                  marginwidth="0"
+                  vspace="0"
+                  hspace="0"
+                  scrolling="yes"
+                  name="patternlibRenderer"
+                  frameborder="0"
+                  @load="iFrameSize"
+          />
+          <div v-else>
+            Please choose a component
+          </div>
+        </transition>
       </div>
     </div>
-    
+
     <code-content :file="activeComponentRender" :get-template="false" />
-    
+
     <hr class="pl-hr">
 
     <div v-if="activeComponentAssets.length" class="pl-content__section">
@@ -115,3 +119,15 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
