dd3ebb0b1fd5f8b39563c88977b0c7d2a839d4db
Martin Herweg <info@martinherweg>
2018-10-06 05:54:09 +0200

    another router component

--- src/assetbundles/gonzo/src/vue/router/index.js
+++ src/assetbundles/gonzo/src/vue/router/index.js
@@ -3,9 +3,19 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '@Views/Home';
+import AppComponent from '@Views/AppComponent';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  routes: [{ path: '/', component: Home }],
+  routes: [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/component/:name',
+      component: AppComponent,
+    },
+  ],
 });
---
+++ src/assetbundles/gonzo/src/vue/views/AppComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pl-component">
+    Das ist eine Komponente mit id von {{ $route.params.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppComponent',
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {},
+};
+</script>
