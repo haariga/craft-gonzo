6b12e5eec0f0f585acfcc47dda025fd789bedd75
Martin Herweg <info@martinherweg>
2018-10-07 05:35:05 +0200

    added component infos component

--- src/assetbundles/gonzo/src/vue/views/AppComponent.vue
+++ src/assetbundles/gonzo/src/vue/views/AppComponent.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="pl-component">
     Das ist eine Komponente mit id von {{ $route.params.name }}
+    <ComponentInfos :infos="infos" />
   </div>
 </template>
 
 <script>
+import ComponentInfos from '@Components/ComponentInfos';
+
 export default {
   name: 'AppComponent',
-  components: {},
+  components: { ComponentInfos },
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    componentData() {
+      const { filelist: { items } = {} } = this.$store.state;
+      const { name } = this.$route.params;
+      const splitName = name.split('.');
+      return items[splitName[0]].children.find(
+        item => item.name === splitName[splitName.length - 1],
+      );
+    },
+    infos() {
+      const { meta } = this.componentData.config;
+      return meta;
+    },
+  },
   watch: {},
   mounted() {},
   created() {},
