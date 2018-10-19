cc774d4a9afc6357581463741576ad3bb3504eec
davidhellmann <david@hellmann.io>
2018-10-19 11:41:34 +0200

    Open new Tab
    Signed-off-by: davidhellmann <david@hellmann.io>

--- src/assetbundles/gonzo/src/vue/components/ComponentRender/iFrame-Area.vue
+++ src/assetbundles/gonzo/src/vue/components/ComponentRender/iFrame-Area.vue
@@ -9,7 +9,7 @@
                 @click="iFrameWidth(width)"
                 v-text="width" />
 
-        <a href="" class="pl-button pl-button--newWindow">Open in new tab</a>
+        <a :href="frameSrc" class="pl-button pl-button--newWindow">Open in new tab</a>
       </div>
     </div>
 
