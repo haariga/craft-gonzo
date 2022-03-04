5d9730ac6a65c99a7ac05c989ef39986c459d996
Martin Herweg <martin@herweg.co>
2022-03-04 01:21:00 +0100

    📦️ update: Update buildchain packages and buildchain itself

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,25 +1,10 @@
 // module exports
 module.exports = {
-  mode: 'jit',
-  purge: {
     content: [
-      '../src/templates/**/*.{twig,html}',
-      './src/vue/**/*.{vue,html}',
+        '../src/templates/**/*.{twig,html,vue}',
+        '../src/vue/**/*.{vue,html}'
     ],
-    layers: [
-      'base',
-      'components',
-      'utilities',
-    ],
-    mode: 'layers',
-    options: {
-      whitelist: [
-        './src/css/components/*.css',
-      ],
-    }
-  },
-  theme: {
-  },
-  corePlugins: {},
-  plugins: [],
+    theme: {},
+    corePlugins: {},
+    plugins: [],
 };
