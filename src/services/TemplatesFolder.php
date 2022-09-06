f56074f3b1a9476c3b1c0b3ebc9f0ed96805912e
Martin Herweg <martin@herweg.co>
2022-09-06 17:11:47 +0200

    🎨 chore: Build a Filetree that can be x levels deep

--- src/services/TemplatesFolder.php
+++ src/services/TemplatesFolder.php
@@ -50,15 +50,23 @@ class TemplatesFolder extends Component
 
     public function readTemplatesFolder()
     {
-        $paths = $this->buildComponentTree(true);
+        $paths = $this->buildComponentTree();
 
         return $paths;
+    }
+
+    private function cleanUp($tree)
+    {
+        $tree = $this->removeEmptyValues($tree);
+        $tree = $this->removeNonSuitableFolders($tree);
+
+        return $tree;
     }
 
     public function getComponents()
     {
         $tree = $this->buildComponentTree();
-        $tree = $this->removeEmptyValues($tree);
+        $tree = $this->cleanUp($tree);
         return $tree;
     }
 
@@ -82,7 +90,43 @@ class TemplatesFolder extends Component
         return $array;
     }
 
-    private function buildComponentTree($full = false)
+    private function array_flatten_with_keys($a, &$collection = []) {
+        if (is_array($a)) {
+            foreach ($a as $key => $item) {
+                array_push($collection, $key);
+                $this->array_flatten_with_keys($item, $collection);
+            }
+        }
+
+       return $collection;
+    }
+
+    private function searchForFiles(array &$children): array
+    {
+        foreach ($children as $key => $child) {
+            $flattened = collect($this->array_flatten_with_keys($child));
+            if ((is_array($child) && array_key_exists('files', $child)) || $flattened->contains('files')) {
+                $children[$key] = $child;
+            } else {
+                unset($children[$key]);
+            }
+        }
+
+        return $children;
+    }
+
+    private function removeNonSuitableFolders(array &$tree): array
+    {
+        foreach ($tree as $key => $item) {
+            if (array_key_exists('children', $item)) {
+                $tree[$key]['children'] = $this->searchForFiles($item['children']);
+            }
+        }
+
+        return $tree;
+    }
+
+    private function buildComponentTree()
     {
         $dir = new RecursiveDirectoryIterator($this->templateDir, FilesystemIterator::SKIP_DOTS);
         $filtered = new RecursiveCallbackFilterIterator($dir, function($current, $key, $iterator) {
@@ -94,6 +138,7 @@ class TemplatesFolder extends Component
 
             return false;
         });
+
 
         $iterator = new RecursiveIteratorIterator($filtered, RecursiveIteratorIterator::SELF_FIRST);
 
@@ -116,7 +161,8 @@ class TemplatesFolder extends Component
                 $parrentAttr[$name]['config'] = [
                     'title' => $name,
                 ];
-                $parrentAttr[$name]['children'] = $this->buildFileTreeV2($fileinfo->getPathname());
+                $subtree = [];
+                $parrentAttr[$name]['children'] = $this->buildFileTree($fileinfo, $subtree);
             }
         }
         unset($parrentAttr);
@@ -124,95 +170,25 @@ class TemplatesFolder extends Component
         return $tree;
     }
 
-    private function buildFileTreeV2(string $dir)
+    private function buildFileTree(\SplFileInfo $dir, array $tree)
     {
-
-        $_dir = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
-
-        $tree = [];
+        $_dir = new \RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
+        $iterator = new RecursiveIteratorIterator($_dir, RecursiveIteratorIterator::SELF_FIRST);
 
         foreach ($_dir as $item) {
-            $name = basename($item->getPathname());
-            $attr = &$tree;
+            $name = $item->getBasename();
             if ($item->isDir()) {
-                $attr[$name] = [];
                 if (!empty(glob($item->getPathname().'/Gonzo*.php'))) {
-                    $attr[$name]['files'][] = $item->getFilename();
+                    $tree[$name] = [];
+                    $tree[$name]['files'] = [];
+                    $tree[$name]['files'] = $this->buildFileTree($item, []);
                 } else {
-                    $attr[$name]['children'] = $this->buildFileTreeV2($item->getPathname());
-                }
-            }
-        }
-
-        $tree = $this->removeEmptyValues($tree);
-
-        unset($attr);
-
-        return $tree;
-    }
-
-
-    private function buildFileTree($dir, $full = false)
-    {
-        $_dir = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
-        $iterator = new RecursiveIteratorIterator($_dir, RecursiveIteratorIterator::SELF_FIRST, RecursiveIteratorIterator::CATCH_GET_CHILD);
-
-        $tree = [];
-        $level = 0;
-
-        foreach ($iterator as $fileinfo) {
-            $name = $fileinfo->getFilename();
-            $path = $fileinfo->getPathname();
-            $type = pathinfo($path, PATHINFO_EXTENSION);
-            $sub_path_name = $iterator->getSubPathName();
-            $parts = explode(DIRECTORY_SEPARATOR, $sub_path_name);
-            array_pop($parts);
-
-            $parrentAttr = &$tree;
-
-            foreach ($parts as $part) {
-                $parrentAttr = &$parrentAttr[$part];
-            }
-
-            if ($fileinfo->isDir()) {
-                if ($iterator->callGetChildren()->isDir()) {
-                    $parrentAttr[$name]['children'] = $this->buildFileTree($fileinfo->getPathname(), $full);
+                    $tree[$name]['children'] = $this->buildFileTree($item, []);
                 }
             } else {
-                if (!$full) {
-                    // TODO: Optimize Condition to only add comps added in the comps array
-                    if (!empty(glob($fileinfo->getPathInfo()->getPathname().'/Gonzo*.php'))) {
-                        if (mb_strpos($name, 'Config')) {
-                            $comps = collect(CraftGonzo::$plugin->getSettings()->comps);
-                            $class = $comps->first(function($value, $key) use ($name) {
-                                $basename = basename($name, '.php');
-                                return $key === $basename;
-                            });
-                            if ($class) {
-                                $class->setPath($_dir->getPathname().DIRECTORY_SEPARATOR);
-                                $class->setTemplatePath($_dir->getSubPathname().DIRECTORY_SEPARATOR);
-                                $parrentAttr['configs'][] = $class;
-                                $parrentAttr['config']['title'] = basename($fileinfo->getPathinfo()->getPathname().DIRECTORY_SEPARATOR);
-                            }
-                        } else {
-                            $parrentAttr['files'][] = [
-                                'filename' => $name,
-                                'filepath' => $path,
-                                'filetype' => $type,
-                            ];
-                        }
-                    }
-                } else {
-                    $parrentAttr['files'][] = [
-                        'filename' => $name,
-                        'filepath' => $path,
-                        'filetype' => $type,
-                    ];
-                }
+                $tree[] = $item;
             }
         }
-
-        unset($parrentAttr);
 
         return $tree;
     }
