27d056203ca68980170c455f86fb788e8767602d
Martin Herweg <martin@herweg.co>
2022-09-12 15:55:44 +0200

    🎨 chore: Add fonts to patternlib file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 module.exports = {
     content: [
         './src/web/assets/src/**/*.{twig,html,vue,pcss,ts}',
-        './src/templates/**/*.{twig}',
+        './src/templates/**/*.twig',
     ],
-    theme: {},
+    theme: {
+        fontFamily: {
+            sans: 'Figtree, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+        },
+    },
     corePlugins: {},
     plugins: [],
 };
