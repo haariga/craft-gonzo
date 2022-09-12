674e3651040b2b38c7487d0afee9ed63f9b7d729
Martin Herweg <martin@herweg.co>
2022-09-12 15:56:52 +0200

    🎨 chore: Add main Store

--- src/web/assets/src/vue/stores/Main.ts
+++ src/web/assets/src/vue/stores/Main.ts
@@ -1,7 +1,26 @@
 import { defineStore } from 'pinia';
+import type { IComponent } from '@/vue/components/Sidebar/SidebarComponent.vue';
+
+export interface ISectionComponent {
+  configs: IComponent[];
+  title: string;
+}
+
+interface ISection {
+  children: ISectionComponent[];
+  config: {
+    title: string;
+  };
+}
+
+interface IState {
+  components: {
+    [key: string]: ISection;
+  };
+}
 
 export const useMainStore = defineStore('main', {
-  state: () => ({ components: {} }),
+  state: (): IState => ({ components: {} }),
   actions: {
     addComponents(components) {
       this.components = components;
