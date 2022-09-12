f5b00999c109bbbb7a14f54942e0e1e480100374
Martin Herweg <martin@herweg.co>
2022-09-12 15:56:31 +0200

    🎨 chore: Add ComponentView Route

---
+++ src/web/assets/src/vue/views/ComponentView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gonzo-componentView">
+    <div class="gonzo-componentHeader bg-white p-10">
+      <h1>{{ title }}</h1>
+      <p class="text-slate-500">
+        {{ store.activeComponent.config.description }}
+      </p>
+    </div>
+    <div class="gonzo-componentRender"></div>
+    <div class="gonzo-componentFiles bg-white p-10"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useActiveComponentStore } from '@/vue/stores/ActiveComponent';
+import { storeToRefs } from 'pinia';
+
+const store = useActiveComponentStore();
+const { title } = storeToRefs(store);
+</script>
